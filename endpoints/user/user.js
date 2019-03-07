ab41366859e486e217e7a5e4b0a121f548b6a4c2
Alando Appling <alandoappling@gmail.com>
2019-03-06 18:11:28 -0800

    Completed endpoints for User

--- endpoints/user/user.js
+++ endpoints/user/user.js
@@ -49,6 +49,24 @@ router.post('/', async (req,res) => {
     } catch (e) {
         res.status(500).json({ error: "There was an error while saving the User to the database" })
     }
+});
+
+router.put('/:id', async (req,res) => {
+    try{
+        const user = await User.update(req.body);
+        res.status(200).json(user)
+    } catch (e) {
+        res.status(500).json({ error: "There was an error while updating the User to the database" })
+    }
+});
+
+router.delete('/:id', async (req,res) => {
+    try{
+        const user = await User.remove(req.params.id);
+        res.status(200).json(user)
+    } catch (e) {
+        res.status(500).json({ error: "There was an error while attempting to delete the User from the database" })
+    }
 })
 
 module.exports = router;
